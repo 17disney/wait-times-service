ec3f52a57ea26573634af96856d759f15a520466
xank <xank@qq.com>
2018-02-16 17:59:34 +0800

    docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /app
 
 RUN npm install --registry=https://registry.npm.taobao.org
 
-EXPOSE 27101
+EXPOSE 80
 
 CMD ["npm", "run", "docker"]
---
+++ app/config/plugin.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+}
+
+exports.validate = {
+  enable: true,
+  package: 'egg-validate'
+}
+
+exports.alinode = {
+  enable: true,
+  package: 'egg-alinode',
+};
