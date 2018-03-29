4c9c1fd9a78ba514de5edbe019b8bf256823d4e6
xank <xank@qq.com>
2018-03-29 23:44:23 +0800

    add schedules cache

--- app/controller/explorer.js
+++ app/controller/explorer.js
@@ -1,5 +1,6 @@
 const Controller = require('egg').Controller
 const moment = require('moment')
+const { lineToObject } = require('../util/api_tool')
 
 // 静态资源控制器
 class ExplorerController extends Controller {
@@ -28,18 +29,68 @@ class ExplorerController extends Controller {
   }
 
   // 时间表
-  async schedules() {
+  async schedulesPre() {
     const { ctx } = this
     const params = ctx.params
     let today = moment().format('YYYY-MM-DD')
     let { local, date = today } = params
 
-    let data = await ctx.service.explorer.schedules.getByLocalDate(local, date)
+    let data = await ctx.service.explorer.schedules.getPreByLocalDate(
+      local,
+      date
+    )
     if (data.length === 2) {
       ctx.body = data
     } else {
       throw new Error('时间表未更新')
     }
+  }
+
+  async schedules() {
+    const { ctx } = this
+    var data
+    const params = ctx.params
+    let today = moment().format('YYYY-MM-DD')
+    let { local, date = today } = params
+
+    data = await ctx.service.explorer.schedules.getByLocalDate(local, date)
+    // 更新至新表
+    if (data.length === 0) {
+      data = await ctx.service.explorer.schedules.getPreByLocalDate(local, date)
+
+      let activities = []
+      for (const item of data) {
+        activities = activities.concat(item.body[0]['activities'])
+      }
+
+      data = []
+
+      for (const item of activities) {
+        let { id, schedule } = item
+        let type = lineToObject(id)['entityType']
+        if (type === 'Entertainment') {
+          if (schedule && schedule.schedules) {
+            let schedules = schedule.schedules
+            if (schedules) {
+              schedules = schedules.filter(_ => _.date === date)
+              let _data = {
+                id,
+                schedules
+              }
+              data.push(_data)
+              await ctx.service.explorer.schedules.updateByLocalDateId(
+                local,
+                date,
+                id,
+                _data
+              )
+            }
+          }
+        }
+      }
+    }
+
+    ctx.body = data
   }
 }
 
--- app/service/explorer/schedules.js
+++ app/service/explorer/schedules.js
@@ -1,7 +1,7 @@
 const Service = require('egg').Service
 
 class SchedulesService extends Service {
-  async getByLocalDate(local, date) {
+  async getPreByLocalDate(local, date) {
     let find = {
       local,
       date
@@ -9,7 +9,39 @@ class SchedulesService extends Service {
     const data = await this.ctx.model.ScanSchedules.find(find, {
       _id: 0
     })
+
     return data
+  }
+  async getByLocalDate(local, date) {
+    let find = {
+      local,
+      date
+    }
+    const data = await this.ctx.model.DsSchedules.find(find, {
+      _id: 0,
+      date: 0,
+      local: 0
+    })
+    return data
+  }
+
+  async updateByLocalDateId(local, date, id, data) {
+    let find = {
+      local,
+      date,
+      id
+    }
+    let ret = await this.ctx.model.DsSchedules.update(
+      find,
+      {
+        $set: data
+      },
+      {
+        upsert: true
+      }
+    )
+
+    return ret
   }
 }
 
