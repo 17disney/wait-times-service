5c69bdf8b4e996d322bad08f454ce6dbf9562c6b
xank <xank@qq.com>
2018-08-29 01:32:10 +0800

    add tz

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ docker rm 17disney-wait-times-service
 
 docker run -d \
   --name 17disney-wait-times-service \
+  -e TZ="Asia/Shanghai" \
   -p 27110:80 \
   --mount type=bind,source=/app/config/wait-times-service,target=/app/config \
   --restart always \
